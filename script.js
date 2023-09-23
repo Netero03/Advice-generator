b70be2ac358468d525fc8a45285f4d89ac560986
NeteroJOD <jatinletsgo@gmail.com>
2023-09-23 17:04:15 +0530

    2nd commit

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.getElementById("dice").addEventListener("click",()=>{
     fetch('https://api.adviceslip.com/advice').then(response => response.json())
       .then(adviceData => {
         // Display the advice on the web page
-        document.getElementById("advice").textContent=adviceData.slip.advice;
-        document.getElementById("id").textContent=adviceData.slip.id;
+        document.getElementById("advice").textContent=advice.textContent.replace("Advice",adviceData.slip.advice);
+        document.getElementById("Num").textContent=Num.textContent.replace("no",adviceData.slip.id);
       })
       .catch(error => {
         console.error('Error:', error);
